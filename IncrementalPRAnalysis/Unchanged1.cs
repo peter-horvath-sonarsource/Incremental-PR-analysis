ffdbdfdd4b7d6a5a74d92399a1c8f827a4878ee0
Costin Zaharia <costin.zaharia@sonarsource.com>
2023-03-16 10:28:36 +0100

    Add code

---
+++ IncrementalPRAnalysis/Unchanged1.cs
@@ -0,0 +1,28 @@
+﻿namespace IncrementalPRAnalysis;
+
+public class Unchanged1
+{
+    // FIXME: S1134 in first unchanged file
+}
+
+public class Test
+{
+    private void UnusedMethod()
+    {
+    }
+
+    public int AnotherUnusedMethod(bool a, bool b)
+    {
+        string unusedVar = null;
+
+        if (a)
+        {
+            if (b)
+            {
+                return 0;
+            }
+        }
+
+        return 1;
+    }
+}
---
+++ IncrementalPRAnalysis/WithChanges.cs
@@ -0,0 +1,23 @@
+﻿namespace IncrementalPRAnalysis;
+
+public class WithChanges
+{
+    private void UnusedMethod()
+    {
+    }
+
+    public int AnotherUnusedMethod(bool a, bool b)
+    {
+        string unusedVar = null;
+
+        if (a)
+        {
+            if (b)
+            {
+                return 0;
+            }
+        }
+
+        return 1;
+    }
+}
